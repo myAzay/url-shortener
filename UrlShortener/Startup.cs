50ef4b04e49561f8a04f61924576e54cf0370d37
myAzay <zaichenkoandrii322@gmail.com>
2021-11-12 17:02:49 +0200

    Added docker support

--- UrlShortener/Startup.cs
+++ UrlShortener/Startup.cs
@@ -103,8 +103,9 @@ namespace UrlShortener
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext dataContext)
         {
+            dataContext.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
