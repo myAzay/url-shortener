8d443401a0b45bd337ad7ec01198540214d2e083
myAzay <zaichenkoandrii322@gmail.com>
2021-11-19 12:33:39 +0200

    added more tests

---
+++ Tests/ShortUrl/Commands/UpdateShortUrlCommandTest.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UrlShortener.Data;
+using UrlShortener.Models;
+using UrlShortener.ShortUrl.Commands;
+using Xunit;
+
+namespace Tests.ShortUrl.Commands
+{
+    public class UpdateShortUrlCommandTest
+    {
+        private ApplicationDbContext GetContext()
+        {
+            return ApplicationDbContextFactory.Create();
+        }
+        [Fact]
+        public async Task ShouldUpdateUrlAndSaveInDatabaseAsync()
+        {
+            Fixture fixture = new Fixture();
+            var command = fixture.Build<UpdateShortUrlCommand>()
+                .With(x => x.UrlId, 1)
+                .Create();
+
+            var context = GetContext();
+
+            fixture.Customize<UpdateShortUrlCommandHandler>(c =>
+                c.FromFactory(() =>
+                new UpdateShortUrlCommandHandler(context))
+            );
+
+            var sut = fixture.Create<UpdateShortUrlCommandHandler>();
+            ShortUrlModel result = await sut.Handle(command, CancellationToken.None);
+            var entity = context.ShortUrlModels.Find(result.Id);
+
+            Assert.NotNull(entity);
+        }
+
+        [Fact]
+        public async Task GivenInvalidIdAndShouldThrowException()
+        {
+            Fixture fixture = new Fixture();
+            var command = fixture.Build<UpdateShortUrlCommand>()
+                .With(x => x.UrlId, 33)
+                .Create();
+
+            var context = GetContext();
+
+            fixture.Customize<UpdateShortUrlCommandHandler>(c =>
+                c.FromFactory(() =>
+                new UpdateShortUrlCommandHandler(context))
+            );
+
+            var sut = fixture.Create<UpdateShortUrlCommandHandler>();
+
+            await Assert.ThrowsAnyAsync<Exception>(() => 
+            sut.Handle(command, CancellationToken.None));
+        }
+    }
+}
---
+++ Tests/ShortUrl/Queries/GetUrlByShortUrlQueryTest.cs
@@ -0,0 +1,68 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UrlShortener.Data;
+using UrlShortener.ShortUrl.Commands;
+using Xunit;
+
+namespace Tests.ShortUrl.Queries
+{
+    public class GetUrlByShortUrlQueryTest
+    {
+        private ApplicationDbContext GetContext()
+        {
+            return ApplicationDbContextFactory.Create();
+        }
+
+        [Fact]
+        public async Task ShouldReturnsCorrectModel()
+        {
+            Fixture fixture = new Fixture();
+            var query = fixture.Build<GetUrlByShortUrlQuery>()
+                .With(x => x.ShortUrl, "http://localhost:5001/qtdJwBun")
+                .Create();
+
+            var context = GetContext();
+
+            fixture.Customize<GetUrlByShortUrlQueryHandler>(c =>
+                c.FromFactory(() =>
+                new GetUrlByShortUrlQueryHandler(context))
+            );
+
+            var sut = fixture.Create<GetUrlByShortUrlQueryHandler>();
+
+            string result = await sut.Handle(query, CancellationToken.None);
+            var entity = context.ShortUrlModels
+                .Where(x => x.ShortUrl == query.ShortUrl)
+                .FirstOrDefault();
+
+            Assert.NotNull(result);
+            Assert.Equal(1,entity.Id);
+        }
+
+        [Fact]
+        public async Task GivenInvalidStringAndShouldThrowException()
+        {
+            Fixture fixture = new Fixture();
+            var query = fixture.Build<GetUrlByShortUrlQuery>()
+                .With(x => x.ShortUrl, "incorrectString")
+                .Create();
+
+            var context = GetContext();
+
+            fixture.Customize<GetUrlByShortUrlQueryHandler>(c =>
+                c.FromFactory(() =>
+                new GetUrlByShortUrlQueryHandler(context))
+            );
+
+            var sut = fixture.Create<GetUrlByShortUrlQueryHandler>();
+
+            await Assert.ThrowsAnyAsync<Exception>(() =>
+            sut.Handle(query, CancellationToken.None));
+        }
+    }
+}
