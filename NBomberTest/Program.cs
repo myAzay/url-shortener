3fcf984906b3b5332fe4d7ae32b462a42dbd0bf3
Andrey <zaichenkoandrii322@gmail.com>
2021-11-23 16:38:59 +0200

    added NBomber support

---
+++ NBomberTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using NBomber.Contracts;
+using NBomber.CSharp;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace NBomberTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var httpClient = new HttpClient();
+
+            var getUrlStep = Step.Create("get-url", async context =>
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync($"https://localhost:5001/api/Url/get-url?ShortUrl=http://localhost:5001/sfdA__Fd");
+
+                    return response.IsSuccessStatusCode
+                        ? Response.Ok()
+                        : Response.Fail();
+
+                }
+                catch (Exception e)
+                {
+                    return Response.Fail();
+                }
+            });
+
+            var getUrlScenario = ScenarioBuilder.CreateScenario("create-url", getUrlStep);
+
+            NBomberRunner
+                .RegisterScenarios(getUrlScenario)
+                .Run();
+        }
+    }
+}
